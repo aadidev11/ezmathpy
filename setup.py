1403768b374988c43f6ba8497f33930c2213d4ed
aadidev11 <98506457+aadidev11@users.noreply.github.com>
2025-08-12 20:45:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,9 @@
-from setuptools import setup, find_packages
-
 setup(
-    name="ezmathpy",
-    version="0.1.0",
-    author="aadidev11",
-    author_email="axdtutorials@gmail.com",  # Replace with your email
-    description="Lightweight Python math utility library",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/aadidev11/ezmathpy.git",  # Replace with your repo URL
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    name='pyeasycalc',  # matches folder name and PyPI package name
+    version='0.2.0',
+    author='aadidev11',
+    description='A simple and easy-to-use math utility library for Python',
+    packages=['pyeasycalc'],
+    install_requires=[],
     python_requires='>=3.6',
 )
