6138fb224cc387a6acf203d0868e73107973a6b0
aadidev11 <98506457+aadidev11@users.noreply.github.com>
2025-08-13 11:55:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 setup(
     name='pyeasycalc',  # matches folder name and PyPI package name
-    version='0.2.2',
+    version='0.2.3',
     author='aadidev11',
     description='A simple and easy-to-use math utility library for Python',
     packages=['pyeasycalc'],
